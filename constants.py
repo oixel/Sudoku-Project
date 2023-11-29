503e258fbb4f34e29c675f39fefb3eb95e1a0a72
oickle <oycell@aol.com>
2023-11-29 10:59:31 -0500

    Move constants to seperate file

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Screen size constants
+WIDTH = 400
+HEIGHT = 400
+
+# Color constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (185, 185, 185)
+
+# Start menu constants
+TITLE_FONT = "assets/Nosifer-Regular.ttf"
+TITLE_FONT_SIZE = 45
+BUTTON_FONT = None
+BUTTON_FONT_SIZE = 20
+BUTTON_WIDTH = 80
+BUTTON_HEIGHT = 35
+
+# Grid constants
+SQUARE_SIZE = 200
+ROWS = 9
+COLUMNS = 9
