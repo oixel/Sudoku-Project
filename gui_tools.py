a5dd788335904c03ff7a0bd3fecc43d2c681d563
oickle <oycell@aol.com>
2023-11-29 12:18:51 -0500

    Comment out code

--- gui_tools.py
+++ gui_tools.py
@@ -2,81 +2,6 @@ import pygame
 from constants import *
 
 
-def draw_grid(view):
-    # Adds extra space created by lines so grid is still accurate
-    extra_space = 0
-
-    # Draws horizontal lines
-    for i in range(1, ROWS):
-        if i % 3 == 0:  # Draws bold lines
-            extra_space += BOLD_LINE_WIDTH
-            pygame.draw.line(
-                view,
-                BLACK,
-                (0, CELL_SIZE * i + extra_space),
-                (WIDTH, CELL_SIZE * i + extra_space),
-                BOLD_LINE_WIDTH
-            )
-        else:  # Draws thinner lines
-            extra_space += THIN_LINE_WIDTH
-            pygame.draw.line(
-                view,
-                BLACK,
-                (0, CELL_SIZE * i + extra_space),
-                (WIDTH, CELL_SIZE * i + extra_space),
-                THIN_LINE_WIDTH
-            )
-
-    extra_space = 0  # Resets extra space
-
-    # Draws vertical lines
-    for i in range(1, ROWS):
-        if i % 3 == 0:  # Draws bold lines
-            extra_space += BOLD_LINE_WIDTH
-            pygame.draw.line(
-                view,
-                BLACK,
-                (CELL_SIZE * i + extra_space, 0),
-                (CELL_SIZE * i + extra_space, HEIGHT),
-                BOLD_LINE_WIDTH
-            )
-        else:  # Draws thinner lines
-            extra_space += THIN_LINE_WIDTH
-            pygame.draw.line(
-                view,
-                BLACK,
-                (CELL_SIZE * i + extra_space, 0),
-                (CELL_SIZE * i + extra_space, HEIGHT),
-                THIN_LINE_WIDTH
-            )
-
-
-def get_extra_space(num):
-    extra_space = 0
-
-    for i in range(1, num):
-        if i % 3 == 0:
-            print('run')
-            extra_space += BOLD_LINE_WIDTH
-        else:
-            print('run', i)
-            extra_space += THIN_LINE_WIDTH
-
-    print(extra_space)
-    return extra_space
-
-
-def draw_number(view, font, num, row, col):
-    extra_x = get_extra_space(col)
-    extra_y = get_extra_space(row)
-
-    num_surface = font.render(str(num), 2, BLACK)
-    x = col * CELL_SIZE + extra_x - (NUMBER_FONT_SIZE / 2)
-    y = row * CELL_SIZE + extra_y - (NUMBER_FONT_SIZE / 2)
-    num_rect = num_surface.get_rect(center=(x, y))
-    view.blit(num_surface, num_rect)
-
-
 # Draw title at center of screen with given offset
 def draw_title(text, x_offset=0, y_offset=0, color=BLACK):
     title = title_font.render(text, True, color)
@@ -99,3 +24,82 @@ def draw_button(view, font, text='', x_offset=0, y_offset=0, label_color=WHITE, 
 
     # Return rectangle to check position
     return button
+
+
+# Draw background grid of 9 9x9 grids with varying line widths
+def draw_grid(view):
+    # Adds extra space created by lines so grid is still accurate
+    extra_space = 0
+
+    # Draws horizontal lines
+    for i in range(1, ROWS):
+        if i % 3 == 0:  # Draws bold grid lines
+            extra_space += BOLD_LINE_WIDTH  # Adds line's width to offset to keep everything accurate
+            pygame.draw.line(
+                view,
+                BLACK,
+                (0, CELL_SIZE * i + extra_space),
+                (WIDTH, CELL_SIZE * i + extra_space),
+                BOLD_LINE_WIDTH
+            )
+        else:  # Draws thinner grid lines
+            extra_space += THIN_LINE_WIDTH  # Adds line's width to offset to keep everything accurate
+            pygame.draw.line(
+                view,
+                BLACK,
+                (0, CELL_SIZE * i + extra_space),
+                (WIDTH, CELL_SIZE * i + extra_space),
+                THIN_LINE_WIDTH
+            )
+
+    extra_space = 0  # Resets extra space
+
+    # Draws vertical lines
+    for i in range(1, ROWS):
+        if i % 3 == 0:  # Draws bold grid lines
+            extra_space += BOLD_LINE_WIDTH  # Adds line's width to offset to keep everything accurate
+            pygame.draw.line(
+                view,
+                BLACK,
+                (CELL_SIZE * i + extra_space, 0),
+                (CELL_SIZE * i + extra_space, HEIGHT),
+                BOLD_LINE_WIDTH
+            )
+        else:  # Draws thinner grid lines
+            extra_space += THIN_LINE_WIDTH  # Adds line's width to offset to keep everything accurate
+            pygame.draw.line(
+                view,
+                BLACK,
+                (CELL_SIZE * i + extra_space, 0),
+                (CELL_SIZE * i + extra_space, HEIGHT),
+                THIN_LINE_WIDTH
+            )
+
+
+# Calculates extra space needed to account for grid lines' widths
+def get_extra_space(num):
+    extra_space = 0
+
+    for i in range(1, num):
+        if i % 3 == 0:  # Bold lines
+            extra_space += BOLD_LINE_WIDTH
+        else:  # Thin lines
+            extra_space += THIN_LINE_WIDTH
+
+    return extra_space  # Return calculated offset
+
+
+# Draw given number in specified row and column
+def draw_number(view, font, num, row, col):
+    # Calculated offset that is needed for the number to be centered in cell
+    extra_x = get_extra_space(col)
+    extra_y = get_extra_space(row)
+
+    # Calculates number's center values
+    x = col * CELL_SIZE + extra_x - (NUMBER_FONT_SIZE / 2)
+    y = row * CELL_SIZE + extra_y - (NUMBER_FONT_SIZE / 2)
+
+    # Renders number in desired row and column
+    num_surface = font.render(str(num), 2, BLACK)
+    num_rect = num_surface.get_rect(center=(x, y))
+    view.blit(num_surface, num_rect)
