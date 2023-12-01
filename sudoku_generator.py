eb97efd2886d3cb62e3c6d42fdfaaa39c7774e98
Kriti <kritishah989@gmail.com>
2023-12-01 18:51:00 -0500

    indent

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -202,10 +202,10 @@ removed is the number of cells to clear (set to 0)
 
 Return: list[list] (a 2D Python list to represent the board)
 '''
-def generate_sudoku(size, removed):
-    sudoku = SudokuGenerator(size, removed)
-    sudoku.fill_values()
-    board = sudoku.get_board()
-    sudoku.remove_cells()
-    board = sudoku.get_board()
-    return board
+    def generate_sudoku(size, removed):
+        sudoku = SudokuGenerator(size, removed)
+        sudoku.fill_values()
+        board = sudoku.get_board()
+        sudoku.remove_cells()
+        board = sudoku.get_board()
+        return board
